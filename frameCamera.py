454a1213d5622083f62dc4e7144d5105ea715ec7
tt405907 <theo.tracol@etu.univ-cotedazur.fr>
2020-09-15 00:12:44 +0200

    Small code cleaning
    Adding comments.
    Small cleaning.

--- frameCamera.py
+++ frameCamera.py
@@ -6,6 +6,7 @@ from PIL import Image
 from PIL import ImageTk
 from PIL import Image as Pil_image, ImageTk as Pil_imageTk
 class FrameCamera(Frame):
+    # For the realization of this class, I was guided by this example for the whole part of capturing the image from the camera and displaying it on the canvas.
     # https://solarianprogrammer.com/2018/04/21/python-opencv-show-video-tkinter-window/
     """ This class will manage the Frame of camera """
     def __init__(self,root,videoSource=0):
@@ -16,21 +17,18 @@ class FrameCamera(Frame):
         self.updateFrame()
 
     def initCameraDevice(self):
+        """ This function allows the instantiation of VideoCapture to activate the camera """
         # If the attribut of cv2 if not detected, CTR + SHIFT + P ->  Preferences: Open Settings (JSON)
         # add "python.linting.pylintArgs": ["--generate-members"]
         self.videoCapture = cv2.VideoCapture(self.videoSource)
-        # Resolution x 
-        #self.videoCapture.set(3,500)
-        # Resolution y 
-        #self.videoCapture.set(4,500)
         if not self.videoCapture.isOpened():
             raise ValueError("Unable to open video source", self.videoSource)
         # Get video source width and height
         self.width = self.videoCapture.get(cv2.CAP_PROP_FRAME_WIDTH)
         self.height = self.videoCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        print("width : " + str(self.width) + " height  : " + str(self.height))
 
     def initCanvas(self):
+        """ This function allows the instantiation of the canvas to store our camera images """
         # init canvas
         self.canvas = tkinter.Canvas(self.root,width= self.width,height = self.height,bd=0, highlightthickness=0, relief='ridge')
         self.canvas.pack()
@@ -38,6 +36,7 @@ class FrameCamera(Frame):
         self.delay = 10
 
     def updateFrame(self):
+        """ This function loops on itself at a certain frequency to take images from our camera """
         retval,frame = self.getFrame()
         if(retval):
             self.photo = Pil_imageTk.PhotoImage(image = Pil_image.fromarray(frame),master=self.root)
