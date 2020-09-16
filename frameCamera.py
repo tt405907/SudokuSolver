f81a88f93a1a8e975349fc2b640ed6b9f732d501
tt405907 <theo.tracol@etu.univ-cotedazur.fr>
2020-09-17 00:00:24 +0200

    Some features have been added
    Implementation of the digit detection model, saved in a file.
    We now manage to extract all the objects from an image looking like a square and extract them from the image. Now we just have to find which ones are sudokus

--- frameCamera.py
+++ frameCamera.py
@@ -6,6 +6,11 @@ from PIL import Image
 from PIL import ImageTk
 from PIL import Image as Pil_image, ImageTk as Pil_imageTk
 import numpy
+from keras.datasets import mnist
+from keras.utils    import to_categorical
+from keras.models   import Sequential, load_model
+from keras.layers   import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+
 class FrameCamera(Frame):
     # For the realization of this class, I was guided by this example for the whole part of capturing the image from the camera and displaying it on the canvas.
     # https://solarianprogrammer.com/2018/04/21/python-opencv-show-video-tkinter-window/
@@ -21,8 +26,8 @@ class FrameCamera(Frame):
     
     def imageTest(self):
         img = Image.open("data/ImagesTest/image6.JPG")
-        img =  img.resize((640,480), Image.ANTIALIAS )
-        img  = img.rotate(270,expand=True)
+        img = img.resize((640,480), Image.ANTIALIAS )
+        img = img.rotate(270,expand=True)
         self.photo = Pil_imageTk.PhotoImage(image=img,master=self.root)
         self.canvas.create_image(0, 0, anchor= tkinter.NW, image= self.photo)
         self.canvas.image = self.photo
@@ -85,7 +90,7 @@ class FrameCamera(Frame):
 
 class ManagerImage:
     def __init__(self):
-        pass
+        self.modelDigits = load_model("data/digits_model.h5")
 
     def preprocessingImage(self,orignalImage):
         self.orignalImage = orignalImage
@@ -111,7 +116,53 @@ class ManagerImage:
         cv2.imshow("Image Countours", self.imageCountours)
         # Step 2 : Get Rectangles
         listRectCorners = self.getRectanglesCorners(contours)
-        listSudokuCorners = self.getSudokuCorners(listRectCorners)
+
+        # Stupid simple 
+        sudokuCorners = self.getSudokuCorners(listRectCorners)
+        sudokuCornersOrdered = self.orderPoints(sudokuCorners)
+        img_width,img_height = self.orignalImage.size
+        imageSudokuCornersOrderedWarp =  self.imageWarp(self.imageUMat.copy(),sudokuCornersOrdered,img_width,img_height)
+        cv2.imshow("imageSudokuCornersOrderedWarp", imageSudokuCornersOrderedWarp)
+        i = 0
+        listRectCornersOrdered = [self.orderPoints(x) for x in listRectCorners]
+        for rectCornersOrdered in listRectCornersOrdered:
+            cv2.imshow("listRectCornersOrdered : " + str(i) , self.imageWarp(self.imageUMat.copy(),rectCornersOrdered,img_width,img_height))
+            i+=1
+
+    def predictionDigit(self,positionCell,image):
+        pass
+
+    def boardSudoku(self,image):
+        """ This function will allow us to extract each number from each cell of the image and return the corresponding board """
+        board = []
+        cellsPosition = self.listPositionsCell() 
+        listRow = []
+        for cellPos in cellsPosition:
+            prediction = self.predictionDigit(cellPos,image)
+            # if last value of the row
+            if(cellPos[0] == 224):
+                board.append(listRow)
+                listRow = []
+        return board
+
+    def listPositionsCell(self):
+        """ This function is returning all the position of Origin to each cells in the image """
+        listPositionsCell = []
+        # Resize image to (252,252) because our model shape(28, 28) -> 28 * 9 = 252
+        #imageSized = image.resize((252,252), Image.ANTIALIAS )
+        x = 0
+        y = 0
+        for i_row in range(0,9):
+            y += 28
+            x = 0
+            for i_column in range(0,9):
+                listPositionsCell.append([x,y])
+                x += 28
+        return listPositionsCell
+
+    def getPrediction(self,image):
+        prediction = self.modelDigits.predict(image)
+        return prediction.argmax()
 
     def getRectanglesCorners(self,contours):
         listRect = []
@@ -132,16 +183,99 @@ class ManagerImage:
     def getSudokuCorners(self,listRectCorners):
         sudokuCorners = []
         sudokuArea = 0
+
         # Simple way (get he biggest) TEMPORARY 
         for corners in listRectCorners:
             areaRect = cv2.contourArea(corners)
             if(areaRect > sudokuArea):
                 sudokuCorners = corners
                 sudokuArea = areaRect
+            
 
         self.imageTemp = self.imageUMat.copy()
         #print("area : " + str(cv2.contourArea(corners)))
         cv2.drawContours(self.imageTemp,sudokuCorners,-1,(255,0,0),10)
         cv2.imshow("Image sudoku Corners", self.imageTemp)
+        return numpy.array(sudokuCorners)
 
+        
+
+    def orderPoints(self,points):
+        """ """
+        pointsReshape = points.reshape((4,2))
+        # New shape of the points
+        newpts =  numpy.zeros((4,2), dtype = "float32")
+        # [ x  y ] -> [x + y]
+        sumPts = pointsReshape.sum(axis=1)
+        # Bottom left
+        newpts[0] = pointsReshape[numpy.argmin(sumPts)]
+        # Top right
+        newpts[3] = pointsReshape[numpy.argmax(sumPts)]
+        
+        diff = numpy.diff(pointsReshape,axis=1)
+        # BOTTOM RIGHT
+        newpts[1] = pointsReshape[numpy.argmax(diff)]
+        # TOP LEFT
+        newpts[2] = pointsReshape[numpy.argmin(diff)]
+        return newpts
+
+    def imageWarp(self,image,pointsOrdered,img_width,img_height):
+        newPoints = numpy.array([[0,0],[0,img_height],[img_width,0],[img_width,img_height]],dtype = "float32")
+        matrix =  cv2.getPerspectiveTransform(pointsOrdered,newPoints)
+        imageWarp = cv2.warpPerspective(image,matrix,(img_width,img_height))
+        return imageWarp
+
+
+    # If you call this method you will run the learning of the model and it will take some times
+    def initModel(self):
+        """ I used this method to generate the model which was going to allow me to make the recognition of the digits """
+        """ Import from : https://www.sitepoint.com/keras-digit-recognition-tutorial/ """
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+        image_index = 35
+        print(y_train[image_index])
+        print(x_train.shape)
+        print(x_test.shape)
+        img_rows, img_cols = 28, 28
+
+        x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+        x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+        x_train=x_train.astype(float)
+        x_test=x_test.astype(float)
+        x_train /= 255
+        x_test /= 255
+
+        num_classes = 10
+
+        y_train = to_categorical(y_train, num_classes)
+        y_test = to_categorical(y_test, num_classes)
+
+
+        model = Sequential()
+        model.add(Conv2D(32, kernel_size=(3, 3),
+            activation='relu',
+            input_shape=(img_rows, img_cols, 1)))
+
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(num_classes, activation='softmax'))
+        model.compile(loss='categorical_crossentropy',
+            optimizer='adam',
+            metrics=['accuracy'])
+        batch_size = 128
+        epochs = 10
+
+        model.fit(x_train, y_train,
+                batch_size=batch_size,
+                epochs=epochs,
+                verbose=1,
+                validation_data=(x_test, y_test))
+        score = model.evaluate(x_test, y_test, verbose=0)
+        print('Test loss:', score[0]) # Test loss: 0.026652975007891655
+        print('Test accuracy:', score[1]) # Test accuracy: 0.9916999936103821
+        model.save("data/digits_model.h5")
 
